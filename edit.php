7c3516b0930280154d1e5bf50d9e4c63ee42368c
diesys <turingsbite@gmail.com>
2021-01-23 22:09:38 +0100

    added php login and edit files

---
+++ edit.php
@@ -0,0 +1,73 @@
+<?php
+
+    function read_village($file_name) {
+        if(file_exists('v/'.$file_name.'.json')) {
+            $json = file_get_contents('v/'.$file_name.'.json');
+            $data = json_decode($json, true);
+            return $data;
+        } else {
+            return FALSE;
+        }
+    }
+
+    function write_village($data, $file_name) {
+        $json = json_encode($data);
+        file_put_contents('v/'.$file_name.'.json', $json);
+    }
+
+    //////////////////////
+    session_start();
+    $error = "";
+    
+    if ($_SESSION['logged_in'] == TRUE and isset($_GET['v'])) {
+        $json = file_get_contents('v/_all.json');
+        $db = json_decode($json, true);
+        
+        // searching village json by id
+        if(array_key_exists($_GET['v'], $db)) {
+            $selected = $db[$_GET['v']];
+            $village = read_village($selected);
+                // var_dump($village);
+        } else {
+            $error = "Village not present!";
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Master lupus</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <center>
+
+<?php if(isset($village)) { ?>
+        <header>
+            <h2>Modifica villaggio <u><?php echo($village['nome']); ?></u></h2>
+            <p>
+                <a href="admin.php">Pannello admin</a>
+                <a href="./?v=<?php echo($village['id']);?>">Bacheca pubblica</a>
+            </p>
+        </header>
+        <!-- <form action="" method="post"> -->
+            <?php var_dump($village); ?>
+        <!-- </form> -->    
+
+<!-- ERRORI -->
+<?php } if ($error != "") { ?>
+    <h2 style="color:yellow;"><?php echo $error;?></h2>
+<?php } ?>
+
+    </center>
+    
+    <footer>
+        <p class="legend">
+            Questo sito conserva solamente i dati caricati dai master (chiedendone il consenso agli utenti): informazioni necessarie al fine del gioco (username/nome riconoscibile degli utenti) e le partite stesse. Le pagine pubbliche di consultazione utilizzano un link generato casualmente per essere visto solo da chi lo possiede.
+        </p>
+    </footer>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Master lupus login</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <center>
+        <form action="admin.php" method="post">
+            <input autofocus type="password" name="password" />
+            <button type="submit">entra*</button>
+            <p>
+                <a href=".">Torna alla home</a>
+            </p>
+        </form>
+
+    </center>
+
+    <footer>
+        <p class="legend">
+           * Effettuando il login La password verrà salvata in un cookie solo per evitare di doverla inserire ad ogni operazione e non viene salvato nulla a insaputa dell'utente, cliccando su "entra" si accetta il suo utilizzo.
+        </p>
+    </footer>
+
+</body>
+</html>
