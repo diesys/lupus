5102f181dc1a5f17912a006826d15743b130a507
diesys <turingsbite@gmail.com>
2021-02-05 04:02:11 +0100

    migliore update nuovo evento

--- update.php
+++ update.php
@@ -10,42 +10,33 @@
         // $days = get_events($village);
     }
 
-    // AGGIUNGI RIMUOVI EVENTO
+    // AGGIUNGI/RIMUOVI EVENTO
     if($_SESSION['logged_in'] == TRUE and isset($village) and isset($_POST)) {
-
-    //// AGGIUNGI EVENTO
+        //// NUOVO EVENTO
         if(isset($_POST['type'])) {
-            // NUOVA NOTTE
+            // NUOVA NOTTE e calcolo giorno
             if($_POST['type'] == "notte") {
                 $day = count($village['giorni']);
                 array_push($village['giorni'], array());
             } else {
-                if(!isset($_POST['day'])) {
-                    $day = count($village['giorni'])-1;
-                } else {
-                    $day = $_POST['day'];
-                }
+                if(!isset($_POST['day'])) { $day = count($village['giorni'])-1; } 
+                else { $day = $_POST['day']; }
+                // nuovo evento da creare (se != notte)
+                $new_event = array('tipo' => $_POST['type']);
 
+                // uccide il giocatore selezioneto (giorno o notte)
                 if(($_POST['type'] == "assassinato" or $_POST['type'] == "giustiziato")) {
-                    // aggiorna il giocatore morto
                     $village['giocatori'] = kill($_POST['player'], $village);
-                } elseif($_POST['type'] == "comunicazione" and isset($_POST['description'])) {
-                    array_push($village['giorni'][$day], array(
-                        'tipo' => $_POST['type'],
-                        'descrizione' => $_POST['description']));
                 }
-            }
 
-            // NUOVA UCCISIONE
-            if(isset($_POST['description']) and isset($_POST['player'])) {
-                if($_POST['type'] != "notte") {
-                    // mettere qui aggiorna giocatore morto
-                    array_push($village['giorni'][$day], array(
-                        'tipo' => $_POST['type'],
-                        'descrizione' => $_POST['description'],
-                        'giocatore' => $_POST['player']));
-                }
-            }        
+                // popola il nuovo evento
+                if(isset($_POST['player'])) { $new_event['giocatore'] = $_POST['player']; }
+                if(isset($_POST['description'])) { $new_event['descrizione'] = $_POST['description']; } 
+                if(isset($_POST['poll_url'])) { $new_event['sondaggio'] = $_POST['poll_url']; }
+
+                // aggiunge l'evento al calendario
+                array_push($village['giorni'][$day], $new_event);
+            }
             write_village($village);
         }
     }
