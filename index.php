54cabc655aa29d4cfe0ca6cbdd6c17a1a0c1d1a3
diesys <turingsbite@gmail.com>
2021-02-20 04:17:31 +0100

    new font

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
     
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <!-- <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet"> -->
-    <link href="https://fonts.googleapis.com/css2?family=Londrina+Solid:wght@100;300;400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Economica:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="assets/css/style.css">
     <?php $seed = rand(0,1);
